e8784e8a2d519026728d8e37801647b830ba0370
chenqiwei <chenqw@techsure.com.cn>
2020-08-20 15:49:41 +0800

    adjust

--- src/main/java/codedriver/module/report/api/SearchReportInstanceApi.java
+++ src/main/java/codedriver/module/report/api/SearchReportInstanceApi.java
@@ -26,63 +26,67 @@ import codedriver.module.report.dto.ReportInstanceVo;
 @Service
 public class SearchReportInstanceApi extends ApiComponentBase {
 
-	@Autowired
-	private ReportInstanceMapper reportInstanceMapper;
+    @Autowired
+    private ReportInstanceMapper reportInstanceMapper;
 
-	@Autowired
-	private TeamMapper teamMapper;
+    @Autowired
+    private TeamMapper teamMapper;
 
-	@Override
-	public String getToken() {
-		return "reportinstance/search";
-	}
+    @Override
+    public String getToken() {
+        return "reportinstance/search";
+    }
 
-	@Override
-	public String getName() {
-		return "查询报表";
-	}
+    @Override
+    public String getName() {
+        return "查询报表";
+    }
 
-	@Override
-	public String getConfig() {
-		return null;
-	}
+    @Override
+    public String getConfig() {
+        return null;
+    }
 
-	@Input({ @Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字", xss = true), @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页"), @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页数量"), @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"), })
-	@Output({ @Param(explode = BasePageVo.class), @Param(name = "tbodyList", desc = "报表列表", explode = ReportInstanceVo[].class) })
-	@Description(desc = "查询报表")
-	@Override
-	public Object myDoService(JSONObject jsonObj) throws Exception {
-		ReportInstanceVo reportInstanceVo = JSONObject.toJavaObject(jsonObj, ReportInstanceVo.class);
-		String userUuid = UserContext.get().getUserUuid(true);
-		// 权限判断：如果是管理员
-		boolean hasAuth = AuthActionChecker.check("REPORT_MODIFY");
-		if (!hasAuth) {
-			// 如果不是管理员，则校验报表权限
-			reportInstanceVo.setSearchMode("user");
-			List<ReportInstanceAuthVo> reportAuthList = new ArrayList<>();
-			reportAuthList.add(new ReportInstanceAuthVo(ReportInstanceAuthVo.AUTHTYPE_USER, userUuid));
-			for (String roleUuid : UserContext.get().getRoleUuidList()) {
-				reportAuthList.add(new ReportInstanceAuthVo(ReportInstanceAuthVo.AUTHTYPE_ROLE, roleUuid));
-			}
-			for (String teamUuid : teamMapper.getTeamUuidListByUserUuid(userUuid)) {
-				reportAuthList.add(new ReportInstanceAuthVo(ReportInstanceAuthVo.AUTHTYPE_TEAM, teamUuid));
-			}
-			reportInstanceVo.setReportInstanceAuthList(reportAuthList);
-		} else {
-			reportInstanceVo.setSearchMode("admin");
-		}
+    @Input({@Param(name = "keyword", type = ApiParamType.STRING, desc = "关键字", xss = true),
+        @Param(name = "needPage", type = ApiParamType.BOOLEAN, desc = "是否需要分页"),
+        @Param(name = "pageSize", type = ApiParamType.INTEGER, desc = "每页数量"),
+        @Param(name = "currentPage", type = ApiParamType.INTEGER, desc = "当前页"),})
+    @Output({@Param(explode = BasePageVo.class),
+        @Param(name = "tbodyList", desc = "报表列表", explode = ReportInstanceVo[].class)})
+    @Description(desc = "查询报表")
+    @Override
+    public Object myDoService(JSONObject jsonObj) throws Exception {
+        ReportInstanceVo reportInstanceVo = JSONObject.toJavaObject(jsonObj, ReportInstanceVo.class);
+        String userUuid = UserContext.get().getUserUuid(true);
+        // 权限判断：如果是管理员
+        boolean hasAuth = AuthActionChecker.check("REPORT_MODIFY");
+        if (!hasAuth) {
+            // 如果不是管理员，则校验报表权限
+            reportInstanceVo.setSearchMode("user");
+            List<ReportInstanceAuthVo> reportAuthList = new ArrayList<>();
+            reportAuthList.add(new ReportInstanceAuthVo(ReportInstanceAuthVo.AUTHTYPE_USER, userUuid));
+            for (String roleUuid : UserContext.get().getRoleUuidList()) {
+                reportAuthList.add(new ReportInstanceAuthVo(ReportInstanceAuthVo.AUTHTYPE_ROLE, roleUuid));
+            }
+            for (String teamUuid : teamMapper.getTeamUuidListByUserUuid(userUuid)) {
+                reportAuthList.add(new ReportInstanceAuthVo(ReportInstanceAuthVo.AUTHTYPE_TEAM, teamUuid));
+            }
+            reportInstanceVo.setReportInstanceAuthList(reportAuthList);
+        } else {
+            reportInstanceVo.setSearchMode("admin");
+        }
 
-		List<ReportInstanceVo> reportInstanceList = reportInstanceMapper.searchReportInstance(reportInstanceVo);
-		JSONObject returnObj = new JSONObject();
-		returnObj.put("tbodyList", reportInstanceList);
-		if (reportInstanceList.size() > 0 && reportInstanceVo.getNeedPage()) {
-			int rowNum = reportInstanceMapper.searchReportInstanceCount(reportInstanceVo);
-			returnObj.put("rowNum", rowNum);
-			returnObj.put("pageSize", reportInstanceVo.getPageSize());
-			returnObj.put("currentPage", reportInstanceVo.getCurrentPage());
-			returnObj.put("pageCount", PageUtil.getPageCount(rowNum, reportInstanceVo.getPageSize()));
-		}
+        List<ReportInstanceVo> reportInstanceList = reportInstanceMapper.searchReportInstance(reportInstanceVo);
+        JSONObject returnObj = new JSONObject();
+        returnObj.put("tbodyList", reportInstanceList);
+        if (reportInstanceList.size() > 0 && reportInstanceVo.getNeedPage()) {
+            int rowNum = reportInstanceMapper.searchReportInstanceCount(reportInstanceVo);
+            returnObj.put("rowNum", rowNum);
+            returnObj.put("pageSize", reportInstanceVo.getPageSize());
+            returnObj.put("currentPage", reportInstanceVo.getCurrentPage());
+            returnObj.put("pageCount", PageUtil.getPageCount(rowNum, reportInstanceVo.getPageSize()));
+        }
 
-		return returnObj;
-	}
+        return returnObj;
+    }
 }
