78f33fd93248e7645f6c10a9217a428ff41300ad
Aienao <laiwt@techsure.com.cn>
2021-01-13 17:06:56 +0800

    --story=1003891 --user=赖文韬 移动OperationTypeEnu... https://www.tapd.cn/54247054/s/1124226

--- src/main/java/codedriver/module/report/api/ExportReportDetailApi.java
+++ src/main/java/codedriver/module/report/api/ExportReportDetailApi.java
@@ -1,7 +1,7 @@
 package codedriver.module.report.api;
 
 import codedriver.framework.common.constvalue.ApiParamType;
-import codedriver.framework.restful.core.constvalue.OperationTypeEnum;
+import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.annotation.Description;
 import codedriver.framework.restful.annotation.Input;
 import codedriver.framework.restful.annotation.OperationType;
