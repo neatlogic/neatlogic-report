b6c14672d784ff71576163924135b87b4e4bb24d
赖文韬 <laiwt@techsure.com.cn>
2021-10-25 15:16:04 +0800

    --story=1005403 --user=赖文韬 苏州银行-发送和解析邮件流传异常时 实现异常信息管理员邮箱的发送配置 https://www.tapd.cn/54247054/s/1184821

--- src/main/java/codedriver/module/report/schedule/plugin/ReportSendJob.java
+++ src/main/java/codedriver/module/report/schedule/plugin/ReportSendJob.java
@@ -6,11 +6,13 @@
 package codedriver.module.report.schedule.plugin;
 
 import codedriver.framework.asynchronization.threadlocal.TenantContext;
+import codedriver.framework.dao.mapper.MailServerMapper;
 import codedriver.framework.dao.mapper.UserMapper;
+import codedriver.framework.dto.MailServerVo;
 import codedriver.framework.dto.UserVo;
+import codedriver.framework.notify.exception.EmailServerNotFoundException;
 import codedriver.framework.scheduler.core.JobBase;
 import codedriver.framework.scheduler.dto.JobObject;
-import codedriver.framework.util.EmailUtil;
 import codedriver.module.report.constvalue.ActionType;
 import codedriver.module.report.dao.mapper.ReportMapper;
 import codedriver.module.report.dao.mapper.ReportSendJobMapper;
@@ -29,9 +31,14 @@ import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.activation.DataHandler;
+import javax.activation.DataSource;
+import javax.annotation.Resource;
+import javax.mail.*;
+import javax.mail.internet.*;
+import javax.mail.util.ByteArrayDataSource;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -43,21 +50,24 @@ import java.util.*;
  */
 @Component
 public class ReportSendJob extends JobBase {
-	static Logger logger = LoggerFactory.getLogger(ReportSendJob.class);
+    static Logger logger = LoggerFactory.getLogger(ReportSendJob.class);
 
-	@Autowired
-	private ReportSendJobMapper reportSendJobMapper;
+    @Resource
+    private ReportSendJobMapper reportSendJobMapper;
 
-	@Autowired
-	private ReportMapper reportMapper;
+    @Resource
+    private ReportMapper reportMapper;
 
-    @Autowired
+    @Resource
     private UserMapper userMapper;
 
-	@Autowired
-	private ReportService reportService;
+    @Resource
+    private MailServerMapper mailServerMapper;
 
-	@Override
+    @Resource
+    private ReportService reportService;
+
+    @Override
     public Boolean isHealthy(JobObject jobObject) {
         ReportSendJobVo jobVo = reportSendJobMapper.getJobBaseInfoById(Long.valueOf(jobObject.getJobName()));
         if (jobVo != null) {
@@ -66,44 +76,44 @@ public class ReportSendJob extends JobBase {
         return false;
     }
 
-	@Override
-	public void reloadJob(JobObject jobObject) {
-		String tenantUuid = jobObject.getTenantUuid();
-		TenantContext.get().switchTenant(tenantUuid);
-		ReportSendJobVo jobVo = reportSendJobMapper.getJobBaseInfoById(Long.valueOf(jobObject.getJobName()));
-		if (jobVo != null && Objects.equals(jobVo.getIsActive(),1)) {
-			JobObject newJobObject = new JobObject.Builder(jobVo.getId().toString(), this.getGroupName(), this.getClassName(), tenantUuid).withCron(jobVo.getCron()).addData("sendJobId", jobVo.getId()).build();
-			schedulerManager.loadJob(newJobObject);
-		} else {
-			schedulerManager.unloadJob(jobObject);
-		}
-	}
+    @Override
+    public void reloadJob(JobObject jobObject) {
+        String tenantUuid = jobObject.getTenantUuid();
+        TenantContext.get().switchTenant(tenantUuid);
+        ReportSendJobVo jobVo = reportSendJobMapper.getJobBaseInfoById(Long.valueOf(jobObject.getJobName()));
+        if (jobVo != null && Objects.equals(jobVo.getIsActive(), 1)) {
+            JobObject newJobObject = new JobObject.Builder(jobVo.getId().toString(), this.getGroupName(), this.getClassName(), tenantUuid).withCron(jobVo.getCron()).addData("sendJobId", jobVo.getId()).build();
+            schedulerManager.loadJob(newJobObject);
+        } else {
+            schedulerManager.unloadJob(jobObject);
+        }
+    }
 
-	@Override
-	public void initJob(String tenantUuid) {
-		/** 初始化所有已激活的发送计划 */
-		List<ReportSendJobVo> jobList = reportSendJobMapper.getAllActiveJob();
-		if(CollectionUtils.isNotEmpty(jobList)){
-			for(ReportSendJobVo vo : jobList){
-				JobObject newJobObject = new JobObject.Builder(vo.getId().toString(), this.getGroupName(), this.getClassName(), tenantUuid).withCron(vo.getCron()).addData("sendJobId",vo.getId()).build();
-				schedulerManager.loadJob(newJobObject);
-			}
-		}
-	}
+    @Override
+    public void initJob(String tenantUuid) {
+        /** 初始化所有已激活的发送计划 */
+        List<ReportSendJobVo> jobList = reportSendJobMapper.getAllActiveJob();
+        if (CollectionUtils.isNotEmpty(jobList)) {
+            for (ReportSendJobVo vo : jobList) {
+                JobObject newJobObject = new JobObject.Builder(vo.getId().toString(), this.getGroupName(), this.getClassName(), tenantUuid).withCron(vo.getCron()).addData("sendJobId", vo.getId()).build();
+                schedulerManager.loadJob(newJobObject);
+            }
+        }
+    }
 
-	@Override
-	public void executeInternal(JobExecutionContext context, JobObject jobObject) throws JobExecutionException {
-		Long id = (Long) jobObject.getData("sendJobId");
-		/** 获取报表发送计划*/
+    @Override
+    public void executeInternal(JobExecutionContext context, JobObject jobObject) throws JobExecutionException {
+        Long id = (Long) jobObject.getData("sendJobId");
+        /** 获取报表发送计划*/
         ReportSendJobVo sendJob = reportSendJobMapper.getJobById(id);
         Map<String, InputStream> reportMap = null;
         String to = null;
         String cc = null;
         boolean canExec = false;
-        if(sendJob != null && Objects.equals(sendJob.getIsActive(),1)) {
+        if (sendJob != null && Objects.equals(sendJob.getIsActive(), 1)) {
             List<ReportSendJobRelationVo> relatedReportList = sendJob.getReportRelationList();
             /** 获取报表 */
-            if(CollectionUtils.isNotEmpty(relatedReportList)) {
+            if (CollectionUtils.isNotEmpty(relatedReportList)) {
                 reportMap = getReport(relatedReportList);
             }
             /** 获取收件人与抄送人 */
@@ -111,39 +121,40 @@ public class ReportSendJob extends JobBase {
             List<String> toEmailList = new ArrayList<>();
             List<String> ccEmailList = new ArrayList<>();
             getReceiverList(receiverList, toEmailList, ccEmailList);
-            if(CollectionUtils.isNotEmpty(toEmailList)) {
+            if (CollectionUtils.isNotEmpty(toEmailList)) {
                 to = String.join(",", toEmailList);
             }
-            if(CollectionUtils.isNotEmpty(ccEmailList)){
+            if (CollectionUtils.isNotEmpty(ccEmailList)) {
                 cc = String.join(",", ccEmailList);
             }
-            if(MapUtils.isNotEmpty(reportMap) && StringUtils.isNotBlank(to)){
+            if (MapUtils.isNotEmpty(reportMap) && StringUtils.isNotBlank(to)) {
                 canExec = true;
             }
         }
-        if(canExec){
+        if (canExec) {
             /** 发送邮件 */
-            try{
-                EmailUtil.sendEmail(sendJob.getEmailTitle(),sendJob.getEmailContent(), reportMap, to, cc);
-            }catch (Exception e){
+            try {
+                sendEmailWithFile(sendJob.getEmailTitle(), sendJob.getEmailContent(), to, cc, reportMap);
+            } catch (Exception e) {
                 throw new JobExecutionException(e.getMessage());
             }
 
-        }else{
+        } else {
             schedulerManager.unloadJob(jobObject);
         }
-	}
+    }
 
     /**
      * 获取收件人与抄送人
      * receiverList包含收件人与抄送人的UUID或email
      * 此方法根据UUID找到用户的email
+     *
      * @param receiverList
      * @param toEmailList
      * @param ccEmailList
      */
     private void getReceiverList(List<ReportReceiverVo> receiverList, List<String> toEmailList, List<String> ccEmailList) {
-        if(CollectionUtils.isNotEmpty(receiverList)){
+        if (CollectionUtils.isNotEmpty(receiverList)) {
             for (ReportReceiverVo vo : receiverList) {
                 if ("to".equals(vo.getType())) { //收件人
                     if (!vo.getReceiver().contains("@")) {
@@ -166,6 +177,7 @@ public class ReportSendJob extends JobBase {
 
     /**
      * 获取报表
+     *
      * @param relatedReportList
      * @return
      */
@@ -177,28 +189,28 @@ public class ReportSendJob extends JobBase {
                 ByteArrayOutputStream os = null;
                 ReportVo report = reportMapper.getReportById(vo.getReportId());
                 if (report != null) {
-                    try{
+                    try {
                         JSONObject paramObj = JSONObject.parseObject(vo.getCondition());
-                        if(MapUtils.isEmpty(paramObj)){
+                        if (MapUtils.isEmpty(paramObj)) {
                             paramObj = new JSONObject();
                         }
                         os = new ByteArrayOutputStream();
                         Map<String, Long> timeMap = new HashMap<>();
-                        Map<String, Object> returnMap = reportService.getQueryResult(report.getId(), paramObj, timeMap, false,null);
+                        Map<String, Object> returnMap = reportService.getQueryResult(report.getId(), paramObj, timeMap, false, null);
                         Map<String, Object> tmpMap = new HashMap<>();
                         Map<String, Object> commonMap = new HashMap<>();
                         tmpMap.put("report", returnMap);
                         tmpMap.put("param", paramObj);
                         tmpMap.put("common", commonMap);
-                        String content = ReportFreemarkerUtil.getFreemarkerExportContent(tmpMap, report.getContent(),ActionType.VIEW.getValue());
+                        String content = ReportFreemarkerUtil.getFreemarkerExportContent(tmpMap, report.getContent(), ActionType.VIEW.getValue());
                         ExportUtil.getPdfFileByHtml(content, true, os);
                         InputStream is = new ByteArrayInputStream(os.toByteArray());
                         reportMap.put(report.getName(), is);
                         os.close();
-                    }catch (Exception e) {
+                    } catch (Exception e) {
                         e.printStackTrace();
-                    }finally {
-                        if(os != null){
+                    } finally {
+                        if (os != null) {
                             try {
                                 os.flush();
                                 os.close();
@@ -213,9 +225,63 @@ public class ReportSendJob extends JobBase {
         return reportMap;
     }
 
+    public void sendEmailWithFile(String title, String content, String to, String cc, Map<String, InputStream> attachmentMap) throws MessagingException, IOException {
+        MailServerVo mailServerVo = mailServerMapper.getActiveMailServer();
+        if (mailServerVo != null && StringUtils.isNotBlank(mailServerVo.getHost()) && mailServerVo.getPort() != null) {
+            /** 开启邮箱服务器连接会话 */
+            Properties props = new Properties();
+            props.setProperty("mail.smtp.host", mailServerVo.getHost());
+            props.setProperty("mail.smtp.port", mailServerVo.getPort().toString());
+            props.put("mail.smtp.auth", "true");
+            Session session = Session.getInstance(props, new Authenticator() {
+                protected PasswordAuthentication getPasswordAuthentication() {
+                    return new PasswordAuthentication(mailServerVo.getUserName(), mailServerVo.getPassword());
+                }
+            });
+
+            MimeMessage msg = new MimeMessage(session);
+            if (StringUtils.isNotBlank(mailServerVo.getFromAddress())) {
+                msg.setFrom(new InternetAddress(mailServerVo.getFromAddress(), mailServerVo.getName()));
+            }
+            /** 设置收件人 */
+            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
+            /** 设置抄送人 */
+            if (StringUtils.isNotBlank(cc)) {
+                msg.setRecipients(Message.RecipientType.CC, InternetAddress.parse(cc, false));
+            }
+            /** 设置邮件标题 */
+            msg.setSubject(title);
+            msg.setSentDate(new Date());
+
+            MimeMultipart multipart = new MimeMultipart();
+            /** 设置邮件正文 */
+            if (StringUtils.isNotBlank(content)) {
+                MimeBodyPart text = new MimeBodyPart();
+                text.setContent(content, "text/plain;charset=UTF-8");
+                multipart.addBodyPart(text);
+            }
+            /** 设置附件 */
+            if (MapUtils.isNotEmpty(attachmentMap)) {
+                for (Map.Entry<String, InputStream> entry : attachmentMap.entrySet()) {
+                    MimeBodyPart messageBodyPart = new MimeBodyPart();
+                    DataSource dataSource = new ByteArrayDataSource(entry.getValue(), "application/pdf");
+                    DataHandler dataHandler = new DataHandler(dataSource);
+                    messageBodyPart.setDataHandler(dataHandler);
+                    messageBodyPart.setFileName(MimeUtility.encodeText(entry.getKey()) + ".pdf");
+                    multipart.addBodyPart(messageBodyPart);
+                }
+            }
+            msg.setContent(multipart);
+            /** 发送邮件 */
+            Transport.send(msg);
+        } else {
+            throw new EmailServerNotFoundException();
+        }
+    }
+
     @Override
-	public String getGroupName() {
-		return TenantContext.get().getTenantUuid() + "-REPORT-SEND";
-	}
+    public String getGroupName() {
+        return TenantContext.get().getTenantUuid() + "-REPORT-SEND";
+    }
 
 }
