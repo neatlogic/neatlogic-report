70783e2eb2eb52d04d097c91285c9b69e06b8525
陈其炜 <chenqw@techsure.com.cn>
2022-04-04 11:21:29 +0800

    adjust
    Signed-off-by: 陈其炜 <chenqw@techsure.com.cn>

--- src/main/java/codedriver/module/report/api/datasource/SearchReportDataSourceDataApi.java
+++ src/main/java/codedriver/module/report/api/datasource/SearchReportDataSourceDataApi.java
@@ -6,13 +6,11 @@
 package codedriver.module.report.api.datasource;
 
 import codedriver.framework.common.constvalue.ApiParamType;
+import codedriver.framework.common.dto.BasePageVo;
 import codedriver.framework.report.dto.ReportDataSourceDataVo;
 import codedriver.framework.report.dto.ReportDataSourceFieldVo;
 import codedriver.framework.report.dto.ReportDataSourceVo;
-import codedriver.framework.restful.annotation.Description;
-import codedriver.framework.restful.annotation.Input;
-import codedriver.framework.restful.annotation.OperationType;
-import codedriver.framework.restful.annotation.Param;
+import codedriver.framework.restful.annotation.*;
 import codedriver.framework.restful.constvalue.OperationTypeEnum;
 import codedriver.framework.restful.core.privateapi.PrivateApiComponentBase;
 import codedriver.module.report.dao.mapper.ReportDataSourceDataMapper;
@@ -51,7 +49,9 @@ public class SearchReportDataSourceDataApi extends PrivateApiComponentBase {
         return null;
     }
 
-    @Input({@Param(name = "dataSourceId", type = ApiParamType.LONG, desc = "数据源id", isRequired = true)})
+    @Input({@Param(name = "dataSourceId", type = ApiParamType.LONG, desc = "数据源id", isRequired = true),
+            @Param(name = "isExpired", type = ApiParamType.INTEGER, desc = "是否过期，0未过期，1已过期")})
+    @Output({@Param(explode = BasePageVo.class)})
     @Description(desc = "查询数据源数据接口")
     @Override
     public Object myDoService(JSONObject jsonObj) throws Exception {
@@ -65,20 +65,30 @@ public class SearchReportDataSourceDataApi extends PrivateApiComponentBase {
             List<HashMap<String, Object>> resultList = reportDataSourceDataMapper.searchDataSourceData(reportDataSourceDataVo);
 
             JSONArray headerList = new JSONArray();
-            JSONObject idHeadObj = new JSONObject();
+           /* JSONObject idHeadObj = new JSONObject();
             idHeadObj.put("key", "id");
             idHeadObj.put("title", "#");
-            headerList.add(idHeadObj);
-            JSONObject insertTimeHeadObj = new JSONObject();
-            insertTimeHeadObj.put("key", "insertTime");
-            insertTimeHeadObj.put("title", "添加日期");
-            headerList.add(insertTimeHeadObj);
+            headerList.add(idHeadObj);*/
+
             for (ReportDataSourceFieldVo fieldVo : reportDataSourceVo.getFieldList()) {
                 JSONObject headObj = new JSONObject();
                 headObj.put("key", "field_" + fieldVo.getId());
                 headObj.put("title", fieldVo.getLabel());
                 headerList.add(headObj);
             }
+
+            JSONObject insertTimeHeadObj = new JSONObject();
+            insertTimeHeadObj.put("key", "insertTime");
+            insertTimeHeadObj.put("title", "同步时间");
+            insertTimeHeadObj.put("type", "time");
+            headerList.add(insertTimeHeadObj);
+
+            JSONObject expiredTimeHeadObj = new JSONObject();
+            expiredTimeHeadObj.put("key", "expireTime");
+            expiredTimeHeadObj.put("title", "过期时间");
+            expiredTimeHeadObj.put("type", "time");
+            headerList.add(expiredTimeHeadObj);
+
             returnObj.put("currentPage", reportDataSourceDataVo.getCurrentPage());
             returnObj.put("pageSize", reportDataSourceDataVo.getPageSize());
             returnObj.put("pageCount", reportDataSourceDataVo.getPageCount());
