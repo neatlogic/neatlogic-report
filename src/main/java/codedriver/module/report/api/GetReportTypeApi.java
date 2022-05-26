b57653688d2c8179f2a0090c3429a3d0af710f1a
赖文韬 <laiwt@techsure.com.cn>
2022-05-26 16:01:04 +0800

    --story=1005985 --user=赖文韬 报表模板管理导入导出和复制功能添加 https://www.tapd.cn/54247054/s/1231339

--- src/main/java/codedriver/module/report/api/GetReportTypeApi.java
+++ src/main/java/codedriver/module/report/api/GetReportTypeApi.java
@@ -51,17 +51,22 @@ public class GetReportTypeApi extends PrivateApiComponentBase {
     public Object myDoService(JSONObject jsonObj) throws Exception {
         JSONArray returnList = new JSONArray();
         List<ReportTypeVo> reportTypeList = reportMapper.getAllReportType();
+        int reportCount = 0;
         for (ReportTypeVo reportTypeVo : reportTypeList) {
             JSONObject obj = new JSONObject();
-            if (reportTypeVo.getReportCount() > 0) {
-                obj.put("label", reportTypeVo.getLabel() + "(" + reportTypeVo.getReportCount() + ")");
-            } else {
-                obj.put("label", reportTypeVo.getLabel());
-            }
+            obj.put("label", reportTypeVo.getLabel());
             obj.put("name", reportTypeVo.getName());
             obj.put("id", reportTypeVo.getName());
+            obj.put("count", reportTypeVo.getReportCount());
             returnList.add(obj);
+            reportCount += reportTypeVo.getReportCount();
         }
+        JSONObject obj = new JSONObject();
+        obj.put("label", "所有");
+        obj.put("name", "all");
+        obj.put("id", "all");
+        obj.put("count", reportCount);
+        returnList.add(obj);
         return returnList;
     }
 }
