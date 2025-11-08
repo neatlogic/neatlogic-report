3891d7c01cd370bcf4a11c92e76455c39e8c67c0
chenqw <gdnpc@163.com>
2025-11-09 00:23:40 +0800

    -[修改]修改LICENSE为SUL
    Signed-off-by: chenqw <gdnpc@163.com>

--- src/main/java/neatlogic/module/report/api/DeleteReportApi.java
+++ src/main/java/neatlogic/module/report/api/DeleteReportApi.java
@@ -1,38 +1,33 @@
-/*Copyright (C) 2024  深圳极向量科技有限公司 All Rights Reserved.
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.*/
+/*
+ *
+ * Copyright (C) 2025  TechSure Co., Ltd.  All Rights Reserved.
+ * This file is part of the NeatLogic software.
+ * Licensed under the NeatLogic Sustainable Use License (NSUL), Version 4.x – 2025.
+ * You may use this file only in compliance with the License.
+ * See the LICENSE file distributed with this work for the full license text.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ */
 
 package neatlogic.module.report.api;
 
+import com.alibaba.fastjson.JSONObject;
 import neatlogic.framework.auth.core.AuthAction;
-import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.common.constvalue.ApiParamType;
+import neatlogic.framework.report.exception.ReportHasBeenQuotedByJobException;
+import neatlogic.framework.report.exception.ReportHasInstanceException;
+import neatlogic.framework.report.exception.ReportNotFoundException;
 import neatlogic.framework.restful.annotation.*;
+import neatlogic.framework.restful.constvalue.OperationTypeEnum;
+import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
 import neatlogic.module.report.auth.label.REPORT_TEMPLATE_MODIFY;
 import neatlogic.module.report.dao.mapper.ReportInstanceMapper;
 import neatlogic.module.report.dao.mapper.ReportMapper;
 import neatlogic.module.report.dao.mapper.ReportSendJobMapper;
-import neatlogic.framework.report.exception.ReportHasBeenQuotedByJobException;
-import neatlogic.framework.report.exception.ReportHasInstanceException;
-import neatlogic.framework.report.exception.ReportNotFoundException;
+import neatlogic.module.report.dto.ReportVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.alibaba.fastjson.JSONObject;
-
-import neatlogic.framework.common.constvalue.ApiParamType;
-import neatlogic.framework.restful.core.privateapi.PrivateApiComponentBase;
-import neatlogic.module.report.dto.ReportVo;
 import org.springframework.transaction.annotation.Transactional;
 
 @AuthAction(action = REPORT_TEMPLATE_MODIFY.class)
